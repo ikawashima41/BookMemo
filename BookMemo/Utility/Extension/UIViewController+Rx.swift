b80ec83bb0757200aafed5546939c9d25bb88fbe
kawashima41 <i.kawashima41@gmail.com>
2019-12-15 05:21:25 +0900

    Add lifecycle extension

---
+++ BookMemo/Utility/Extension/UIViewController+Rx.swift
@@ -0,0 +1,30 @@
+import UIKit
+import RxCocoa
+import RxSwift
+import RxRelay
+
+extension Reactive where Base: UIViewController {
+    var viewWillAppear: Observable<Void> {
+        return sentMessage(#selector(base.viewWillAppear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+
+    var viewDidAppear: Observable<Void> {
+        return sentMessage(#selector(base.viewDidAppear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+
+    var viewWillDisappear: Observable<Void> {
+        return sentMessage(#selector(base.viewWillDisappear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+
+    var viewDidDisappear: Observable<Void> {
+        return sentMessage(#selector(base.viewDidDisappear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+}
