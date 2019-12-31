5caadd1d1ad622bf5cca20c0089621658fe9b299
kawashima41 <i.kawashima41@gmail.com>
2019-12-31 19:39:48 +0900

    Add SignInViewModel UnitTest

--- BookMemoTests/ViewModel/SignInViewModelTests.swift
+++ BookMemoTests/ViewModel/SignInViewModelTests.swift
@@ -9,6 +9,7 @@
 import XCTest
 import RxTest
 import RxSwift
+import DataManager
 
 @testable import BookMemo
 
@@ -19,23 +20,64 @@ class SignInViewModelTests: XCTestCase {
     var scheduler: TestScheduler!
 
     override func setUp() {
-
+        let dataStore = AccountDataStoreMock()
+        viewModel = SignInViewModel(dependency: dataStore)
+        scheduler = TestScheduler(initialClock: 1)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func test_validateSignInForm() {
+        let emailTextobserbavle = scheduler.createHotObservable([
+            Recorded.next(5, "email"),
+            Recorded.next(15, "eamil@gmail.com")
+            ]).asObservable()
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let passwordTextObservable = scheduler.createHotObservable([
+            Recorded.next(10, ""),
+            Recorded.next(20, "password")
+            ]).asObservable()
+
+        let input = SignInViewModel.Input(didSignInButtonTapped: Observable.never(), didSignupButtonTapped: Observable.never(), emailText: emailTextobserbavle, passwordText: passwordTextObservable)
+
+        let output = viewModel.transform(input: input)
+
+        let obserber = scheduler.createObserver(Bool.self)
+
+        output.isValid
+            .bind(to: obserber)
+            .disposed(by: disposeBag)
+
+        scheduler.start()
+
+
+        let expectedEvent = [
+            Recorded.next(10, false),
+            Recorded.next(15, false),
+            Recorded.next(20, true)
+        ]
+
+        XCTAssertEqual(expectedEvent, obserber.events)
+
+    }
+}
+
+extension SignInViewModelTests {
+
+    class AccountDataStoreMock: AccountDataStore {
+
+        func SignIn(with info: AuthModel) -> Observable<SignInAPI.Response> {
+            return Observable.never()
+        }
+
+        func signUp(with info: AuthModel) -> Observable<SignUpAPI.Response> {
+            return Observable.never()
+        }
+
+        func SignOut() -> Observable<SignOutAPI.Response> {
+            return Observable.never()
         }
     }
-
 }
